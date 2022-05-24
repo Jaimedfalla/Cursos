1e454be96cc3d473f58b16dc0271752679bdce4a
Jaime E Falla <65088216+Jaimedfalla@users.noreply.github.com>
2022-05-24 17:20:28 -0500

    Práctica de composition API

--- JavaScript/vuejs/cashflow/src/App.vue
+++ JavaScript/vuejs/cashflow/src/App.vue
@@ -1,17 +1,38 @@
 <template>
-    <home></home>
+<!-- Con el coomponente suspense podemos cargar de manera asíncrona el componente home y mientras
+esta transición se da, se muestra el componente splash screen -->
+  <suspense>
+    <template #default>
+      <home></home>
+    </template>
+    <template #fallback>
+      <splash-screen></splash-screen>
+    </template>
+  </suspense>
 </template>
 
-<script>
-import Home from './components/Home.vue';
+<script setup>
+import SplashScreen from './components/SplashScreen.vue';
+import { defineAsyncComponent } from 'vue';
 
-export default{
-  components:{
-    Home
-  }
-}
+const home = defineAsyncComponent(()=>
+  new Promise((resolve)=>{
+    setTimeout(() => {
+        resolve(import('@/components/Home.vue'))
+      },
+      2500);
+  }));
 </script>
-
 <style>
-
+html,
+body,
+.app {
+  min-height: 100vh;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+* {
+  --brand-green: #04b500;
+  --brand-blue: #0689b0;
+}
 </style>
--- JavaScript/vuejs/cashflow/src/components/Home.vue
+++ JavaScript/vuejs/cashflow/src/components/Home.vue
@@ -1,3 +1,22 @@
 <template>
-    
+    <Layout>
+        <template #header>
+            <Header></Header>
+        </template>
+        <template #resume>
+            <Resume :label="'Mi Ahorro'" :total-amount="amount"></Resume>
+        </template>
+        <template #movements>
+            <Movements></Movements>
+        </template>
+    </Layout>
 </template>
+<script setup>
+import Layout from './Layout.vue';
+import Header from './Header.vue';
+import Resume from './Resume/Index.vue';
+import Movements from './Movements.vue';
+import { ref } from 'vue';
+
+const amount = ref(2000000);
+</script>
